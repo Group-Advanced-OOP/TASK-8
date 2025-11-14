24eded4ea2aa44bb3305113665e11a2569d99c10
unknown <Yuusuflaki82@gmail.com>
2025-11-14 16:13:56 +0300

    Commit 1
    I define prepareFormPanel() to build the input form using a 3x2 GridLayout.
    - I set the background color and padding using setBackground() and setBorder().
    - I add the name label and name textfield to the form panel by calling prepareJLabelNamelabel() and prepareJTextFieldNametextfield().
    - I add the email label and email textfield using prepareJLabelEmailLabel() and prepareJTextFieldEmailTextField().
    - I add an empty JLabel as a spacer to align the save button visually.
    - I add the Save button created by prepareJButtonSaveButton().
    - I return the assembled form panel.
    
    - I implement prepareJLabelNamelabel():
      - I declare namelabel as a JLabel with the text “NAME:”.
      - I apply Comic Sans MS font with bold style and size 16.
      - I set a red foreground color using new Color(200, 0, 0).
      - I return this styled JLabel.

--- TASK_8 Code/src/LMS.java
+++ TASK_8 Code/src/LMS.java
@@ -19,7 +19,7 @@ public class LMS {
     public LMS() {
         this.prepareJFrame();
     }
-    
+
     public JFrame prepareJFrame() {
         Mainframe = new JFrame("STUDENTS FORM");
         Mainframe.setSize(600, 450);
@@ -30,4 +30,27 @@ public class LMS {
         Mainframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Mainframe.setVisible(true);
         return Mainframe;
+    }
+
+    public JPanel prepareFormPanel() {
+        Formpanel = new JPanel(new GridLayout(3, 2, 10, 10));
+        Formpanel.setBackground(new Color(255, 245, 235));
+        Formpanel.setBorder(BorderFactory.createEmptyBorder(20, 30, 20, 30));
+
+        Formpanel.add(this.prepareJLabelNamelabel());
+        Formpanel.add(this.prepareJTextFieldNametextfield());
+        Formpanel.add(this.prepareJLabelEmailLabel());
+        Formpanel.add(this.prepareJTextFieldEmailTextField());
+        Formpanel.add(new JLabel(""));
+        Formpanel.add(this.prepareJButtonSaveButton());
+
+        return Formpanel;
+    }
+
+    // Name label and field
+    public JLabel prepareJLabelNamelabel() {
+        namelabel = new JLabel("NAME:");
+        namelabel.setFont(new Font("Comic Sans MS", Font.BOLD, 16));
+        namelabel.setForeground(new Color(200, 0, 0));
+        return namelabel;
     }
